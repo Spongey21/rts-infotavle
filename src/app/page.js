b50368b14c9cea39238e05c7504f8a5adb4eb2ea
Spongey21 <driton.latifi101@gmail.com>
2024-01-23 23:52:35 +0100

    font awesome bus icons added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 "use client"
 
+import Transport from '@/components/transport'
+
 export default function Home() {
   return (
     <main className='h-[100vh]'>
       <div className='float-right w-2/5 h-full bg-gray-600 -z-20'>
+        <Transport/>
       </div>
     </main>
   );
