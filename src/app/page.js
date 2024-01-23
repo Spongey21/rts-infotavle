c80316fdd1aee036550a77c38aaa71a06f85b477
Spongey21 <driton.latifi101@gmail.com>
2024-01-23 11:13:27 +0100

    removed code from wrong branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
 "use client"
 
-import Weather from '@/components/weather'
-
 export default function Home() {
-  
-
   return (
     <main className='h-[100vh]'>
       <div className='float-right w-2/5 h-full bg-gray-600 -z-20'>
-        <Weather/>
       </div>
     </main>
   );
