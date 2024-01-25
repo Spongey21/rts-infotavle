d2fd985cedf78a8bc51ca2323eb5544e7752a6fc
Spongey21 <driton.latifi101@gmail.com>
2024-01-25 12:03:45 +0100

    Adjusted transport table colors

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Transport from '@/components/transport'
 export default function Home() {
   return (
     <main className='h-[100vh]'>
-      <div className='float-right w-2/5 h-full bg-gray-600 -z-20'>
+      <div className='float-right w-2/5 h-full -z-20'>
         <Transport/>
       </div>
     </main>
--- src/components/transport.js
+++ src/components/transport.js
@@ -5,33 +5,33 @@ import { faBus } from '@fortawesome/free-solid-svg-icons'
 
 export default function Transport() {
     return (
-        <table className="w-full h-1/2">
+        <table className="w-full h-1/2 border-2 border-black bg-gray-200">
             <thead>
-                <tr className="h-[50px]">
-                    <th className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,2vw,7em)]">Type</th>
-                    <th className="border-2 border-black w-[30%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,2vw,7em)]">Ankomst</th>
-                    <th className="border-2 border-black w-[30%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,2vw,7em)]">Slut</th>
-                    <th className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,2vw,7em)]">Forsinkelse</th>
+                <tr className="bg-[#0C0C0C] text-gray-200">
+                    <th className="w-[20%] h-[8vh] text-2xl text-center font-semibold [font-size:_clamp(1em,2vw,3em)]">Type</th>
+                    <th className="w-[30%] h-[8vh] text-2xl text-center font-semibold [font-size:_clamp(1em,2vw,3em)]">Ankomst</th>
+                    <th className="w-[30%] h-[8vh] text-2xl text-center font-semibold [font-size:_clamp(1em,2vw,3em)]">Slut</th>
+                    <th className="w-[20%] h-[8vh] text-2xl text-center font-semibold [font-size:_clamp(1em,2vw,3em)]">Forsinkelse</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-5xl text-center [font-size:_clamp(1em,3vw,7em)]"><FontAwesomeIcon icon={faBus}></FontAwesomeIcon></td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">9:30</td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">9:45</td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">+5</td>
+                <tr className="border-2 border-black">
+                    <td className="w-[20%] h-[8vh] text-5xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold"><FontAwesomeIcon icon={faBus}></FontAwesomeIcon></td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">9:30</td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">9:45</td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">+5</td>
                 </tr>
-                <tr>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-5xl text-center [font-size:_clamp(1em,3vw,7em)]"><FontAwesomeIcon icon={faBus}></FontAwesomeIcon></td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">9:30</td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">9:45</td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">+5</td>
+                <tr className="border-2 border-black">
+                    <td className="w-[20%] h-[8vh] text-5xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold"><FontAwesomeIcon icon={faBus}></FontAwesomeIcon></td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">9:30</td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">9:45</td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">+5</td>
                 </tr>
-                <tr>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-5xl text-center [font-size:_clamp(1em,3vw,7em)]"><FontAwesomeIcon icon={faBus}></FontAwesomeIcon></td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">9:30</td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">9:45</td>
-                    <td className="border-2 border-black w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,7em)]">+5</td>
+                <tr className="border-2 border-black">
+                    <td className="w-[20%] h-[8vh] text-5xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold"><FontAwesomeIcon icon={faBus}></FontAwesomeIcon></td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">9:30</td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">9:45</td>
+                    <td className="w-[20%] h-[8vh] text-2xl text-center [font-size:_clamp(1em,3vw,4em)] font-bold">+5</td>
                 </tr>
             </tbody>
         </table>
