deaa7fc03dca5ea534beb50492d27d0ccabe2116
Spongey21 <driton.latifi101@gmail.com>
2024-01-26 08:48:14 +0100

    Reset Production branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,8 @@
 "use client"
 
-import Transport from '@/components/transport'
-
 export default function Home() {
   return (
     <main className='h-[100vh]'>
-      <div className='float-right w-2/5 h-full bg-gray-600 -z-20'>
-        <Transport/>
-      </div>
     </main>
   );
 }
