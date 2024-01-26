cb71a8194ba1f2284280cd086eb5907d2207c1ed
Spongey21 <driton.latifi101@gmail.com>
2024-01-26 08:20:12 +0100

    removed redundant classname

--- src/components/transport.js
+++ src/components/transport.js
@@ -5,7 +5,7 @@ export default function Transport() {
     return (
         <table className="w-full h-1/2 border-2  uppercase  bg-white">
             <thead>
-                <tr className="bg-neutral-100  text-white text-gray-500  rounded-lg">
+                <tr className="bg-neutral-100 text-gray-500  rounded-lg">
                     <th className="w-1/5 h-[8vh] text-2xl text-center  font-medium">Type</th>
                     <th className="w-3/10 h-[8vh] text-2xl text-center font-medium">Ankomst</th>
                     <th className="w-3/10 h-[8vh] text-2xl text-center font-medium">Slut</th>
