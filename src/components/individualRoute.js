e5d503352c66a9c610704cf4737784fc7120d102
Spongey21 <driton.latifi101@gmail.com>
2024-01-30 22:40:19 +0100

    Transport items centered and aligned to fit properly

--- src/components/individualRoute.js
+++ src/components/individualRoute.js
@@ -9,32 +9,32 @@ export default function IndividualRoute({ id, delay, arrival, departure }) {
     useEffect(() => {
         (async () => {
             await animate([
-                [scope.current, { width: '27%' }, { duration: 2 }],
-                [scope.current, { width: '33%' }, { duration: 2, delay: 2 }],
+                [scope.current, { width: '300%' }, { duration: 2 }],
+                [scope.current, { width: '500%' }, { duration: 2 }],
                 [scope.current, { backgroundColor: '#34b233' }]
             ])
 
-            await scope.current.parentElement.parentElement.remove()
+            //await scope.current.parentElement.parentElement.remove()
         })()
     }, [])
 
     return (
         <tr className="text-center border-2">
-            <td className="justify-center w-1/5 h-[8vh] font-medium">
+            <td className="h-[8vh]">
+                <span className="fixed flex justify-center items-center h-1/5 w-[8%] text-4xl font-medium">{id}</span>
                 <motion.div
-                    className='absolute -z-10 top-[8%] h-[21%] bg-neutral-100 resize-x'
+                    className='-z-10 h-full w-0 bg-neutral-100'
                     ref={scope}
                 />
-                <span className="w-1/5 h-[8vh] text-4xl font-medium">{id}</span>
             </td>
-            <td className="w-1/5 h-[8vh] text-4xl font-medium">{delay}</td>
-            <td className="w-3/10 h-[8vh] font-medium">
+            <td className="h-[8vh] text-4xl font-medium">{delay}</td>
+            <td className="h-[8vh] font-medium">
                 <div className='flex flex-col'>
                     <span className='text-4xl'>9:28</span>
                     <span className='text-sm'>Pulsen 8, Roskilde</span>
                 </div>
             </td>
-            <td className="w-3/10 h-[8vh] text-4xl font-medium">
+            <td className="h-[8vh] text-4xl font-medium">
                 <div className='flex flex-col'>
                     <span className='text-4xl'>9:45</span>
                     <span className='text-sm'>Roskilde St</span>
