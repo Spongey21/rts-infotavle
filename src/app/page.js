abbfa2e08f868e5222260799eefccce258e7ee89
Stoyanski <danchevstoyan@gmail.com>
2024-02-01 08:42:59 +0100

    basic layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 "use client"
 
+import Schedule from '@/components/schedule'
+
+
 export default function Home() {
   return (
     <main className='h-[100vh]'>
+    <Schedule/>
     </main>
   );
 }
---
+++ src/components/schedule.js
@@ -0,0 +1,72 @@
+export default function Transport() {
+    return (
+        <table className="w-[70%] h-1/2 border-2  uppercase  bg-white">
+            <thead>
+                <tr className="bg-neutral-100 bg-white rounded-lg">
+                    <th className="w-[15%] h-[8vh] text-center border-2 border-black font-medium">
+                    <div className="flex flex-col">
+                        <span className="text-2xl">WU</span>
+                        <span className="text-[0.7rem]">webudvikling</span>
+                    </div>
+                    </th>
+                    <th className="w-[15%] h-[8vh] text-center border-2 border-black font-medium">
+                    <div className="flex flex-col">
+                        <span className="text-2xl">DM</span>
+                        <span className="text-[0.7rem]">Digital Media</span>
+                    </div>
+                    </th>
+                    <th className="w-[15%] h-[8vh] text-center border-2 border-black font-medium">
+                    <div className="flex flex-col">
+                        <span className="text-2xl">MG</span>
+                        <span className="text-[0.7rem]">Medie Grafik</span>
+                    </div>
+                    </th>
+                    <th className="w-[15%] h-[8vh] text-center border-2 border-black font-medium">
+                    <div className="flex flex-col">
+                        <span className="text-2xl">TD</span>
+                        <span className="text-[0.7rem]">Teknisk Design</span>
+                    </div>
+                    </th>
+                    <th className="w-[20%] h-[8vh] text-center border-2 border-black font-medium">
+                    <div className="flex flex-col">
+                        <span className="text-2xl">GF1</span>
+                        <span className="text-[0.7rem]">Grund Forløb 1</span>
+                    </div>
+                    </th>
+                    <th className="w-[20%] h-[8vh] text-center border-2 border-black font-medium">
+                    <div className="flex flex-col">
+                        <span className="text-2xl">Brobyg</span>
+                        <span className="text-[0.7rem]">Brobygning</span>
+                    </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="text-center border-2 ">
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[20%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[20%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                </tr>
+                <tr className="text-center border-2">
+                <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-1 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[20%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[20%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                </tr>
+                <tr className="text-center border-2 ">
+                <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[15%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[20%] h-[8vh] text-4xl border-2 border-black font-medium"></td>
+                    <td className="w-[20%] h-[8vh] text-4xl border-2 border-black font-medium"></td>  
+                </tr>
+            </tbody>
+        </table>
+    );
+}
