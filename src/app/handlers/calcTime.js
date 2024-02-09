2685655ada165104a1656e1d32bbf4cd5ff93a26
Spongey21 <driton.latifi101@gmail.com>
2024-02-09 10:43:12 +0100

    removed console log from calcTime

--- src/app/handlers/calcTime.js
+++ src/app/handlers/calcTime.js
@@ -8,7 +8,6 @@ function getRealDate(day) {
 }
 
 export function unixInSeconds(time) {
-    console.log(time.split(':')[0] + ':' + time.split(':')[1] + ':' + '00');
     const today = new Date()
     const arrival = new Date(`${today.getFullYear()}-${getRealMonth(today.getMonth())}-${getRealDate(today.getDate())}T${getRealDate(time.split(':')[0])}:${time.split(':')[1]}:00+01:00`)
 
