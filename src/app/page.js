65bd386313556d313a588ae020a2048b4f30bcdd
Spongey21 <106562685+Spongey21@users.noreply.github.com>
2024-02-27 11:59:33 +0000

    items placed correctly

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Transport from "@/components/transport";
 
 export default function Home() {
   return (
-    <main className='h-[100vh]'>
+    <main className='h-screen flex'>
       <Schedule/>
       <div className='float-right w-2/5 h-full -z-20'>
         <Transport />
