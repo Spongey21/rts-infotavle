327efce29ea788af390872bf317b18436c80ab48
Spongey21 <106562685+Spongey21@users.noreply.github.com>
2024-03-01 07:21:35 +0000

    removed destinations under time

--- src/components/individualRoute.js
+++ src/components/individualRoute.js
@@ -39,14 +39,8 @@ export default function IndividualRoute({ id, arrTime, arrDest, depTime, depDest
     return (
         <motion.article className="flex items-center justify-center w-full h-full text-black border-b-2 border-blue-950" ref={scope}>
             <h1 className="text-4xl text-center w-1/3">{id}</h1>
-            <div className="flex flex-col items-center justify-center w-1/3">
-                <span className="text-4xl text-center">{arrTime}</span>
-                <span className="text-sm text-center">{arrDest}</span>
-            </div>
-            <div className="flex flex-col w-1/3">
-                <span className="text-4xl text-center">{depTime}</span>
-                <span className="text-sm text-center">{depDest}</span>
-            </div>
+                <span className="text-3xl text-center w-1/3">{arrTime}</span>
+                <span className="text-3xl text-center w-1/3">{arrDest.split(',')[0]}</span>
         </motion.article>
     )
 }
