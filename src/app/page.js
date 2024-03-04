8a9affc1c3da6bb338889fab62daa108d6d3ccc0
Spongey21 <106562685+Spongey21@users.noreply.github.com>
2024-03-04 11:49:45 +0000

    footer placement adjusted

--- src/app/page.js
+++ src/app/page.js
@@ -8,18 +8,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="h-[15vh] text-center capitalize flex flex-col justify-center">
-        <h1 className="text-4xl">velkommen til pulsen 8</h1>
-        <h2 className="text-3xl">indgang A/B</h2>
+      <header className="h-[17.5vh] text-center capitalize flex flex-col justify-center">
+        <h1 className="text-6xl">velkommen til pulsen 8</h1>
+        <h2 className="text-4xl">indgang A/B</h2>
       </header>
-      <main className="h-[70vh] w-[96%] m-auto flex justify-between">
+      <main className="h-[65vh] w-[96%] m-auto flex justify-between">
         <Schedule/>
         <div className="float-right w-[40%] h-full">
           <Transport /> 
         </div>
       </main>
-      <footer className="h-[15vh] w-5/6 m-auto flex justify-between">
-        <Image src="/RTS-logo.png" width={70} height={0} alt="RTS logo" style={{height: '60%', margin: 'auto 0'}}/>
+      <footer className="h-[17.5vh] w-[96%] m-auto flex justify-between items-center">
+        <Image src="/RTS-logo.png" width={100} height={0} alt="RTS logo" style={{height: '80%'}}/>
         <Clock/>
       </footer>
     </>
