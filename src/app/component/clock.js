4844870374ed08bb0a945c6b994d23b277d60e16
Spongey21 <106562685+Spongey21@users.noreply.github.com>
2024-03-04 11:49:34 +0000

    clock sizes increased

--- src/app/component/clock.js
+++ src/app/component/clock.js
@@ -30,12 +30,12 @@ export default function Clock() {
     }, [])
 
     return (
-        <div className="flex text-white text-2xl items-center">
-            <div className="bg-blue-950 w-[50px] h-[50px] flex items-center justify-center rounded shadow-xl">{time && time[0]}</div>
+        <div className="flex items-center text-white text-4xl">
+            <div className="bg-blue-950 w-[70px] h-[60px] flex items-center justify-center rounded shadow-xl">{time && time[0]}</div>
             <span className="w-[10px] text-black text-center">:</span>
-            <div className="bg-blue-950 w-[50px] h-[50px] flex items-center justify-center rounded shadow-xl">{time && time[1]}</div>
+            <div className="bg-blue-950 w-[70px] h-[60px] flex items-center justify-center rounded shadow-xl">{time && time[1]}</div>
             <span className="w-[10px] text-black text-center">:</span>
-            <div className="bg-blue-950 w-[50px] h-[50px] flex items-center justify-center rounded shadow-xl">{time && time[2]}</div>
+            <div className="bg-blue-950 w-[70px] h-[60px] flex items-center justify-center rounded shadow-xl">{time && time[2]}</div>
         </div>
     )
 }
