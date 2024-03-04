871f54e44e2c4f4d0624bb6cdee1f06880234466
Spongey21 <106562685+Spongey21@users.noreply.github.com>
2024-03-04 11:23:29 +0000

    fixed spacing and logo width/height

--- src/app/page.js
+++ src/app/page.js
@@ -8,19 +8,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="h-[10vh] text-center capitalize flex flex-col justify-center">
+      <header className="h-[15vh] text-center capitalize flex flex-col justify-center">
         <h1 className="text-4xl">velkommen til pulsen 8</h1>
         <h2 className="text-3xl">indgang A/B</h2>
       </header>
-      <main className="h-[80vh] w-[96%] m-auto flex justify-between">
+      <main className="h-[70vh] w-[96%] m-auto flex justify-between">
         <Schedule/>
         <div className="float-right w-[40%] h-full">
           <Transport /> 
         </div>
       </main>
-      <footer className="h-[10vh] w-5/6 m-auto flex justify-between">
-        {/* <img className="h-[80%] mt-auto mb-auto" src="./RTS-logo.png"/> */}
-        <Image src="/RTS-logo.png" width={50} height={0} alt="RTS logo" style={{height: '80%', aspectRatio: 1, margin: 'auto 0'}}/>
+      <footer className="h-[15vh] w-5/6 m-auto flex justify-between">
+        <Image src="/RTS-logo.png" width={70} height={0} alt="RTS logo" style={{height: '60%', margin: 'auto 0'}}/>
         <Clock/>
       </footer>
     </>
