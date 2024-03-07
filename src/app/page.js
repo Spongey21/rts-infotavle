3ebbe7a25a5fc31211958977de0ac104ff082b9f
Spongey21 <106562685+Spongey21@users.noreply.github.com>
2024-03-07 12:06:51 +0000

    slightly more space for main content

--- src/app/page.js
+++ src/app/page.js
@@ -8,17 +8,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="h-[17.5vh] text-center capitalize flex flex-col justify-center">
+      <header className="h-[15vh] text-center capitalize flex flex-col justify-center">
         <h1 className="text-6xl">velkommen til pulsen 8</h1>
         <h2 className="text-4xl">indgang A/B</h2>
       </header>
-      <main className="h-[65vh] w-[96%] m-auto flex justify-between">
+      <main className="h-[70vh] w-[96%] m-auto flex justify-between">
         <Schedule/>
         <div className="float-right w-[40%] h-full">
           <Transport /> 
         </div>
       </main>
-      <footer className="h-[17.5vh] w-[96%] m-auto flex justify-between items-center">
+      <footer className="h-[15vh] w-[96%] m-auto flex justify-between items-center">
         <Image src="/RTS-logo.png" width={100} height={0} alt="RTS logo" style={{height: '80%'}}/>
         <Clock/>
       </footer>
